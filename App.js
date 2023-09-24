cb62e6f975c3783a8677a512cae703556d4fb023
Marwan Bukhori <marwan@terato.com>
2023-09-24 10:36:43 +0800

    Setup Redux Toolkit

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react'
+import { Provider } from 'react-redux'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import RootNavigation from './src/navigations/RootNavigation'
+import { store } from './src/store'
 
 function App() {
   return (
+    <Provider store={store}>
     <SafeAreaProvider>
       <RootNavigation />
     </SafeAreaProvider>
+  </Provider>
   );
 }
 
---
+++ src/store.js
@@ -0,0 +1,23 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { pokemonApi } from './services/nilam-api';
+
+export const store = configureStore({
+  reducer: {
+    // Add the generated reducer as a specific top-level slice
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
+});
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export var RootState = store.getState;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export var AppDispatch = store.dispatch;
